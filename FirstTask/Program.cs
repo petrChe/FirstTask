05b19ba2e0f8b1ec720638694119547ec878f671
petrChe <petrchebanenko28@mail.ru>
2016-06-29 00:34:26 +0600

    https://github.com/petrChe/FirstTask.git

--- FirstTask/Program.cs
+++ FirstTask/Program.cs
@@ -3,13 +3,44 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
 
 namespace FirstTask
 {
     class Program
     {
+        static double xCoordinate;
+        static double yCoordinate;
         static void Main(string[] args)
         {
+            Console.Clear();
+            //for (int i = 0; i < 3;)
+            //{
+            //    Console.WriteLine("Please write the first coordinate: ");
+            //    double xCoordinate = Convert.ToDouble(Console.ReadLine());
+            //    Console.WriteLine("Please write the second coordinate: ");
+            //    double yCoordinate = Convert.ToDouble(Console.ReadLine());
+            //    Console.WriteLine("X: " + xCoordinate + " Y: " + yCoordinate);
+            //    Console.ReadLine();
+            //    Console.Clear();
+            //    i++;
+            //}
+            StreamReader streamReader = File.OpenText(@"K:\Epam C Sharp dot net\textFile.txt");
+            string line;
+            while ((line = streamReader.ReadLine()) != null)
+            {
+                string[] doubles = line.Split(' ');
+                    for (int j = 0; j < doubles.Length;j++)
+                    {
+                       if(j % 2 != 0)
+                        {
+                            yCoordinate = double.Parse(doubles[j]);
+                            xCoordinate = double.Parse(doubles[j-1]);
+                            Console.WriteLine("X: " + xCoordinate + " Y: " + yCoordinate);
+                    }
+                }
+            }
+            Console.ReadLine();
         }
     }
 }
---
+++ WpfApplicationFirstTask/MainWindow.xaml.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.IO;
+
+namespace WpfApplicationFirstTask
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        double xCoordinate;
+        double yCoordinate;
+
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            StreamReader streamReader = File.OpenText(@"K:\Epam C Sharp dot net\textFile.txt");
+            string line;
+            while ((line = streamReader.ReadLine()) != null)
+            {
+                string[] doubles = line.Split(' ');
+                for (int j = 0; j < doubles.Length; j++)
+                {
+                    if (j % 2 != 0)
+                    {
+                        yCoordinate = double.Parse(doubles[j]);
+                        xCoordinate = double.Parse(doubles[j - 1]);
+                    }
+                }
+            }
+        }
+
+        private void textBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            xCoordinate = double.Parse(textBox.Text);
+        }
+
+        private void textBox1_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            yCoordinate = double.Parse(textBox1.Text);
+        }
+
+        private void button_Click(object sender, RoutedEventArgs e)
+        {
+            textBox2.Text = "X: " + xCoordinate + " Y: " + yCoordinate;
+        }
+    }
+}
